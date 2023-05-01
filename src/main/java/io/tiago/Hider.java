553458d7807b7cf716998f7edf3906cdcb2c3f16
Tiago <tiagotg.ribeiro@gmail.com>
2023-05-01 20:20:08 -0300

    renamed files

--- src/main/java/io/tiago/Hider.java
+++
@@ -1,8 +0,0 @@
-package io.tiago;
-
-public interface Hider {
-    void help();
-    void init() throws Exception;
-    String enc(String input, Argument argument) throws Exception;
-    String dec(String input, Argument argument) throws Exception;
-}
