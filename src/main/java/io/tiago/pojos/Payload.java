1169a9138e6895dd4fa9cf32bee165fa9834bed2
Tiago <tiagotg.ribeiro@gmail.com>
2023-05-01 20:20:34 -0300

    created payload pojo for input

---
+++ src/main/java/io/tiago/pojos/Payload.java
@@ -0,0 +1,18 @@
+package io.tiago.pojos;
+
+public class Payload {
+    
+    private String content;
+
+    public Payload(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
