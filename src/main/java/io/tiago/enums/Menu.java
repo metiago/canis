7c7aba2c306aeed20a9b48445ecb5e48f23cda3a
Tiago <tiagotg.ribeiro@gmail.com>
2023-05-02 18:30:04 -0300

    changed menu label

--- src/main/java/io/tiago/enums/Menu.java
+++ src/main/java/io/tiago/enums/Menu.java
@@ -7,7 +7,7 @@ public enum Menu {
     ENC(String.format("%s%-10s%s%n", "enc", " ", "Encrypt input value.")),
     DEC(String.format("%s%-10s%s%n", "dec", " ", "Decrypt input value.")),
     ARGUMENTS("General Options:"),
-    ARG_F(String.format("%s%-10s%s%n", "--f", " ", "Optional - save message to file."));
+    ARG_F(String.format("%s%-10s%s%n", "--p", " ", "Save text to a .txt file."));
 
     private String value;
 
