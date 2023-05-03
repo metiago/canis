60a061367075f7a9f8477aa59f8f7b3225241073
Tiago <tiagotg.ribeiro@gmail.com>
2023-05-03 20:03:31 -0300

    delete useless object

---
+++ src/main/java/io/tiago/transformers/Constants.java
@@ -0,0 +1,8 @@
+package io.tiago.transformers;
+
+public class Constants {
+    
+    public static final String VECTOR_FILE = "canis_vector.txt";
+    public static final String SECRET_KEY_FILE = "canis_secret_key.txt";
+    public static final String SALT_FILE = "canis_salt.txt";
+}
